b11587ed1aa9e4f6320a133740a0ed1b7b1f0744
ferdousalam007 <iamferdousalam007@gmail.com>
2021-08-05 12:17:29 +0600

    loop facctorial

---
+++ factorialLoop.js
@@ -0,0 +1,16 @@
+//factoral for loop
+var factorial = 1;
+
+for(let i = 1; i<=10 ; i++){
+    factorial = factorial*i
+    console.log(factorial);
+}
+
+//factorial with whilie loop
+var factorial2 = 1;
+var j=1;
+while (j<=10){
+    factorial2 = factorial2*j;
+    j++;
+    console.log(factorial2);
+}
