a3404a3ea8104061858249570c9641eedeec5b98
ferdousalam007 <iamferdousalam007@gmail.com>
2021-08-05 13:12:32 +0600

    function factorial complete

---
+++ fuctionfactorial.js
@@ -0,0 +1,27 @@
+//for loop factorial function
+// function factorialOne(inputFactinput){
+//     var fact = 1;
+// for(var i= 1; i<=inputFactinput; i++){
+//     fact = fact*i;
+// }
+// return fact;
+
+// }
+// var inputnumber = factorialOne(5);
+// console.log(inputnumber);
+
+//while loop factorial function
+
+function one(inputnumberOne){
+        var factWhile = 1;
+        var k = 1;  
+        while(k<=inputnumberOne){
+        factWhile = factWhile*k;
+        k++;
+        }
+        return factWhile;
+
+}
+var inputnumber1= 10;
+var inputnumberOne =one(inputnumber1);
+console.log(inputnumberOne); 
